242d979218de8abf14b3f9c94d005278e7a3d324
Denis Mitrofanov <denis.mitr@gmail.com>
2018-11-03 00:57:27 +0300

    starts on tech page

---
+++ resources/lang/en/tech.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'php' => 'PHP is a popular general-purpose scripting language that is especially suited to web development.',
+    'laravel' => 'The PHP Framework For Web Artisans',
+    'js' => 'JavaScript (JS) is a lightweight interpreted or JIT-compiled programming language with first-class functions',
+    'csshtml' => 'HTML5 - CSS3',
+    'vue' => 'The Progressive JavaScript Framework',
+    'python' => 'Python is a programming language that lets you work quickly and integrate systems more effectively',
+];
--- resources/views/front/tech.blade.php
+++ resources/views/front/tech.blade.php
@@ -1,23 +1,72 @@
 @extends('layouts.front')
 
+@section('keyboard')
+    @include('front.components._keyboard')
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Tech</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="w-full min-h-full">
+        <article class="container mx-auto py-2">
+            @if(app()->isLocale('ru'))
+                <h3 class="border-b-2 border-white border-solid py-2 mb-2 text-grey text-4xl">
+                    Самые <span class="text-red">современные</span> технологии
+                </h3>
+            @else
+                <h3 class="border-b-2 border-white border-solid py-2 mb-2 text-grey text-4xl">
+                    My <span class="text-red">favorite</span> technologies
+                </h3>
+            @endif
+        </article>
+        <section class="w-full py-20">
+            <div class="container mx-auto py-2 mb-20">
+                <div class="flex flex-wrap mb-4 -mx-2">
+                    <div class="sm:w-1/2 md:w-1/3 lg:w-1/4 h-auto px-2">
+                        <img class="h-48" src="{{ asset('img/php.png') }}" alt="@lang('tech.php')">
+                        <h3 class="mb-2"><a class="text-red hover:text-red-darker text-2xl" target="_blank" href="https://php.net">PHP</a></h3>
+                        <p class="text-grey">@lang('tech.php')</p>
+                    </div>
+                    <div class="sm:w-1/2 md:w-1/3 lg:w-1/4 h-auto px-2">
+                        <img class="h-48" src="{{ asset('img/laravel-logo.png') }}" alt="@lang('tech.laravel')">
+                        <h3 class="mb-2"><a class="text-red hover:text-red-darker text-2xl" target="_blank" href="https://laravel.com">Laravel</a></h3>
+                        <p class="text-grey">@lang('tech.laravel')</p>
+                    </div>
+                    <div class="sm:w-1/2 md:w-1/3 lg:w-1/4 h-auto px-2">
+                        <img class="h-48" src="{{ asset('img/js.jpg') }}" alt="@lang('tech.js')">
+                        <h3 class="mb-2"><a class="text-red hover:text-red-darker text-2xl" target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</a></h3>
+                        <p class="text-grey">@lang('tech.js')</p>
+                    </div>
+                    <div class="sm:w-1/2 md:w-1/3 lg:w-1/4 h-auto px-2">
+                        <img class="h-48" src="{{ asset('img/css-html.png') }}" alt="@lang('tech.csshtml')">
+                        <h3 class="mb-2"><a class="text-red hover:text-red-darker text-2xl" target="_blank" href="https://www.w3.org/">HTML5 - CSS3</a></h3>
+                        <p class="text-grey">@lang('tech.csshtml')</p>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <div class="container mx-auto py-2 mb-20">
+                <div class="flex flex-wrap mb-4 -mx-2">
+                    <div class="w-1/2 md:w-1/3 lg:w-1/4 h-auto px-2">
+                        <img class="h-48" src="{{ asset('img/vuejs.png') }}" alt="@lang('tech.vue')">
+                        <h3 class="mb-2"><a class="text-red hover:text-red-darker text-2xl" target="_blank" href="https://vuejs.org/">VueJS</a></h3>
+                        <p class="text-grey">@lang('tech.vue')</p>
+                    </div>
+                    <div class="w-1/2 md:w-1/3 lg:w-1/4 h-auto px-2">
+                        <img class="h-48" src="{{ asset('img/python.png') }}" alt="@lang('tech.python')">
+                        <h3 class="mb-2"><a class="text-red hover:text-red-darker text-2xl" target="_blank" href="https://www.python.org/">Python</a></h3>
+                        <p class="text-grey">@lang('tech.python')</p>
+                    </div>
+                    <div class="w-1/2 md:w-1/3 lg:w-1/4 h-auto px-2">
+                        <img class="h-48" src="{{ asset('img/js.jpg') }}" alt="@lang('tech.js')">
+                        <h3 class="mb-2"><a class="text-red hover:text-red-darker text-2xl" target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</a></h3>
+                        <p class="text-grey">@lang('tech.js')</p>
+                    </div>
+                    <div class="w-1/2 md:w-1/3 lg:w-1/4 h-auto px-2">
+                        <img class="h-48" src="{{ asset('img/css-html.png') }}" alt="@lang('tech.csshtml')">
+                        <h3 class="mb-2"><a class="text-red hover:text-red-darker text-2xl" target="_blank" href="https://www.w3.org/">HTML5 - CSS3</a></h3>
+                        <p class="text-grey">@lang('tech.csshtml')</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
-</div>
 @endsection
